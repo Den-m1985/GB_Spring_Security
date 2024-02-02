a969bb9b412422455de0539f6b91815c6fb685fd
User <den_m1985@mail.ru>
2024-02-02 16:15:10 +0200

    first work version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,13 @@
-package board.board;
+package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoardApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BoardApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
---
+++ src/main/java/org/example/controller/HomeController.java
@@ -0,0 +1,29 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "redirect:/";
+    }
+
+    @GetMapping("/public-data")
+    public String user() {
+        return "public-data";
+    }
+
+    @GetMapping("/private-data")
+    public String admin() {
+        return "private-data";
+    }
+
+}
