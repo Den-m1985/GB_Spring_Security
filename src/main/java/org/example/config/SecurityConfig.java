22bd56505f27b6fb2ff917b24d82e0b06f38cf1b
Den <den_m1985@mail.ru>
2024-02-04 12:31:00 +0200

    dont finish frontend

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -1,42 +1,56 @@
 package org.example.config;
 
+
+import lombok.AllArgsConstructor;
+import org.example.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@AllArgsConstructor
+public class SecurityConfig{
+
+	private final UserService userService;
+
 
 	@Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/private-data").hasRole("ADMIN")
-				.antMatchers("/public-data").authenticated()
-				.and()
-				.formLogin()
-				.and()
-				.logout()
-				.logoutSuccessUrl("/login");
-        return http.build();
-    }
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests((authorize) -> authorize
+						.requestMatchers("/index").permitAll()
+						.anyRequest().authenticated()
+				);
+//				.formLogin(login -> login
+//						.defaultSuccessUrl("/login").permitAll())
+//				.logout(logout -> logout.logoutSuccessUrl("/logout"));
+		return http.build();
+	}
+
+
+//	@Bean
+//	public AuthenticationProvider authenticationProvider() {
+//		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//		authProvider.setUserDetailsService(userService.userDetailsService());
+//		authProvider.setPasswordEncoder(passwordEncoder());
+//		return authProvider;
+//	}
 	
 	@Bean
 	PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		//return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		return new BCryptPasswordEncoder();
 	}
-	
-	@Bean
-	UserDetailsManager inMemoryUserDetailsManager() {
-		var user1 = User.withUsername("user").password("{noop}password").roles("USER").build();
-		var user2 = User.withUsername("admin").password("{noop}password").roles("USER", "ADMIN").build();
-		return new InMemoryUserDetailsManager(user1, user2);
-	}
+
+//	@Bean
+//	UserDetailsManager inMemoryUserDetailsManager() {
+//		var user1 = User.withUsername("user").password("{noop}password").roles("USER").build();
+//		var user2 = User.withUsername("admin").password("{noop}password").roles("USER", "ADMIN").build();
+//		return new InMemoryUserDetailsManager(user1, user2);
+//	}
 }
--- src/main/java/org/example/controller/HomeController.java
+++ src/main/java/org/example/controller/HomeController.java
@@ -1,9 +1,12 @@
 package org.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/auth")
 public class HomeController {
 
     @GetMapping("/")
@@ -11,10 +14,6 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("/index")
-    public String index() {
-        return "redirect:/";
-    }
 
     @GetMapping("/public-data")
     public String user() {
@@ -24,6 +23,22 @@ public class HomeController {
     @GetMapping("/private-data")
     public String admin() {
         return "private-data";
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignUpForm(Model model) {
+        //model.addAttribute("signUpRequest");
+        return "sign-up";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/index")
+    public String showIndexPage() {
+        return "index"; // Имя Thymeleaf шаблона (без расширения .html)
     }
 
 }
